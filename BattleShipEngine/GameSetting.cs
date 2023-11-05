98440baca99d1ae2955047b7a5cf143bfbcc9415
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2023-11-05 10:58:03 +0100

    boatcount doc

--- BattleShipEngine/GameSetting.cs
+++ BattleShipEngine/GameSetting.cs
@@ -5,11 +5,11 @@ namespace BattleShipEngine;
 /// </summary>
 /// <param name="Height">Height of the board</param>
 /// <param name="Width">Width of the board</param>
-/// <param name="BoatCount">todo</param>
+///<param name="BoatCount">The 0th element is the amount of <c>1 tile long boats</c>, the 1th element is the amount of <c>2 tile long boats</c>, ...</param>
 public readonly record struct GameSetting(
     int Height,
     int Width,
-    int[] BoatCount /*Todo: This needs some doc, what does the array mean?*/)
+    int[] BoatCount)
 {
     /// <summary>
     /// Settings that can be found here <see href="https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7"></see>
